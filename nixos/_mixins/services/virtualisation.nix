d1d4f41dfba0715c275b0443c433f74a2bfedde8
Joshua Nestler <developer.jn@mailbox.org>
2023-06-18 08:58:35 +0200

    fix flake

--- nixos/_mixins/services/virtualisation.nix
+++ nixos/_mixins/services/virtualisation.nix
@@ -1,11 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.docker = {
-    enable = true;
-    enableOnBoot = true;
-  };
-
   virtualisation.virtualbox.host = {
     enable = true;
     enableExtensionPack = true;
--- nixos/harmony/hardware.nix
+++ nixos/harmony/hardware.nix
@@ -5,6 +5,8 @@
     inputs.nixos-hardware.nixosModules.common-cpu-intel
     inputs.nixos-hardware.nixosModules.common-gpu-intel
     inputs.nixos-hardware.nixosModules.common-gpu-nvidia
+    inputs.nixos-hardware.nixosModules.common-pc
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
     ../_mixins/services/pipewire.nix
   ];
 
@@ -16,10 +18,39 @@
     packages = [ pkgs.cascadia-code ];
   };
 
+  fileSystems."/" =
+    {
+      device = "/dev/nvme1n1p3";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/nvme1n1p1";
+      fsType = "vfat";
+    };
+
+  fileSystems."/home" =
+    {
+      device = "/dev/nvme1n1p4";
+      fsType = "btrfs";
+    };
+
+  swapDevices =
+    [{ device = "/dev/nvme1n1p2"; }];
+
   hardware = {
+
     nvidia = {
       nvidiaSettings = false;
       modesetting.enable = true;
+
+      prime = {
+        offload.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
     };
     opengl = {
       enable = true;
