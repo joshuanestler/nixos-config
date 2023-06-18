bbde085c92c522e6f6d832ca4ef7e941f02e1c4c
Joshua Nestler <developer.jn@mailbox.org>
2023-06-18 09:14:29 +0200

    font management not yet working

--- nixos/_mixins/desktop/default.nix
+++ nixos/_mixins/desktop/default.nix
@@ -12,34 +12,8 @@
   fonts = {
     fontDir.enable = true;
     fonts = with pkgs; [
-      (nerdfonts.override { fonts = [ "FiraCode" "UbuntuMono" ]; })
-      joypixels
-      liberation_ttf
-      ubuntu_font_family
-      work-sans
       cascadia-code
     ];
-
-    fontconfig = {
-      antialias = true;
-      cache32Bit = true;
-      defaultFonts = {
-        serif = [ "Work Sans" "Joypixels" ];
-        sansSerif = [ "Work Sans" "Joypixels" ];
-        monospace = [ "FiraCode Nerd Font Mono" ];
-        emoji = [ "Joypixels" ];
-      };
-      enable = true;
-      hinting = {
-        autohint = false;
-        enable = true;
-        style = "hintslight";
-      };
-      subpixel = {
-        rgba = "rgb";
-        lcdfilter = "light";
-      };
-    };
   };
 
   # Accept the joypixels license
